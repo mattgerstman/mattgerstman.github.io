a58725fcfb52cd11795adfff4a459120e30d0867
Matthew Gerstman <matthew.gerstman@gmail.com>
2018-05-28 18:53:52 -0400

    Switch to hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,22 @@
-import * as React from "react";
-import { Provider } from "react-redux";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import * as styles from "./App.css";
-import getStore from "./data/store";
-import { checkForRedirect } from "./gh-pages-utils";
-import { LandingPage } from "./pages/landing-page";
-import RecordPage from "./pages/record-page";
+import * as React from 'react';
+import {Provider} from 'react-redux';
+import {HashRouter as Router, Route} from 'react-router-dom';
+import * as styles from './App.css';
+import getStore from './data/store';
+import {LandingPage} from './pages/landing-page';
+import RecordPage from './pages/record-page';
 
-class App extends React.Component<{}, {}> {
-  constructor(props) {
-    super(props);
-    checkForRedirect();
-  }
-  render() {
-    return (
-      <Provider store={getStore()}>
-        <Router>
-          <div className={styles.container}>
-            <Route exact={true} path="/" component={LandingPage} />
-            <Route path="/records" component={RecordPage} />
-          </div>
-        </Router>
-      </Provider>
-    );
-  }
+function App() {
+  return (
+    <Provider store={getStore()}>
+      <Router>
+        <div className={styles.container}>
+          <Route exact={true} path="/" component={LandingPage} />
+          <Route path="/records" component={RecordPage} />
+        </div>
+      </Router>
+    </Provider>
+  );
 }
 
 export default App;
--- src/gh-pages-utils.ts
+++
@@ -1,7 +0,0 @@
-export function checkForRedirect() {
-  const { location } = window;
-  const urlParams = new URLSearchParams(location.search);
-  if (urlParams.get("redirect") === "true") {
-    // location.replace(urlParams.get("pathname")!);
-  }
-}
