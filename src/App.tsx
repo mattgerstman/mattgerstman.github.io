85990ec9cf08f1b2fc69fae9603b3b5cb5e372c1
Matthew Gerstman <mattgerstman@gmail.com>
2018-05-28 14:44:07 -0400

    Add landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import * as React from 'react';
-import {Provider} from 'react-redux';
-import getStore from './data/store';
-import RecordPage from './pages/record-page';
+import * as React from "react";
+import * as styles from "./App.css";
+import getStore from "./data/store";
+import RecordPage from "./pages/record-page";
+import { Provider } from "react-redux";
+import { LandingPage } from "./pages/landing-page";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <Provider store={getStore()}>
-      <RecordPage />
+      <Router>
+        <div className={styles.container}>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/records" component={RecordPage} />
+        </div>
+      </Router>
     </Provider>
   );
 }
