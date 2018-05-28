b64f6431010d228334cb32c2281781c9d66b7219
Matthew Gerstman <mattgerstman@gmail.com>
2018-05-28 17:32:06 -0400

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import * as styles from "./App.css";
 import getStore from "./data/store";
+import { checkForRedirect } from "./gh-pages-utils";
 import { LandingPage } from "./pages/landing-page";
 import RecordPage from "./pages/record-page";
-import { checkForRedirect } from "./gh-pages-utils";
 
 class App extends React.Component<{}, {}> {
   constructor(props) {
